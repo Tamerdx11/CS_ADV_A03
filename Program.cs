d5bf773c8839930ec8428b11fcc520e7e4000fe6
tamer <tamernull400@gmail.com>
2025-08-02 23:19:31 +0200

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CS_ADV_A03;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        Console.ReadKey();
+    }
+}
