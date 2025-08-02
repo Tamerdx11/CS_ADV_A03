e78d03d1a91eafdac34cffb7076a1041f0bb6b7c
tamer <tamernull400@gmail.com>
2025-08-03 00:19:32 +0200

    fix assignment issues

--- Program.cs
+++ Program.cs
@@ -4,9 +4,76 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Book> books = [
+            new("978-3-16-148410-0", "C# Programming", ["John ", " Doe"], new DateTime(2020, 1, 1), 29.99m),
+            new("978-1-23-456789-7", "Advanced C#", ["Jane ", " Smith"], new DateTime(2021, 5, 15), 39.99m),
+            new("978-0-12-345678-9", "C# in Depth", ["Mark ", " Johnson"], new DateTime(2019, 8, 20), 49.99m)
+        ];
+        // a: User-Defined Delegate
+        BookDelegate titleDelegate = BookFunctions.GetTitle;
+        Console.WriteLine("----------------- Books Titles -----------------");
+        //LibraryEngine.ProccessBook(books, titleDelegate); // This line is commented out to avoid ambiguity with the Func<Book, string> overload
+        LibraryEngine.ProccessBook(books, new Func<Book, string>(titleDelegate));
 
+        // b Built-in Delegate
+        Console.WriteLine("----------------- Books Authors -----------------");
+        LibraryEngine.ProccessBook(books, BookFunctions.GetAuthors);
+
+        // c: Anonymous Function
+        Console.WriteLine("----------------- Books ISBN -----------------");
+        LibraryEngine.ProccessBook(books, delegate(Book b) { return b.ISBN; });
+
+        // d: Lambda Expression
+        Console.WriteLine("----------------- Books Publication Date -----------------");
+        LibraryEngine.ProccessBook(books, (b) => b.PublicationDate.ToString());
 
         Console.ReadKey();
     }
+}
+
+public class Book
+{
+    public string ISBN { get; set; } = default!;
+    public string Title { get; set; } = default!;
+    public string[] Author { get; set; } = default!;
+    public DateTime PublicationDate { get; set; } 
+    public decimal Price { get; set; }
+
+    public Book(string _ISBN, string _Title, string[] _Author, DateTime _PublicationDate, decimal _Price)
+    {
+        ISBN = _ISBN;
+        Title = _Title;
+        Author = _Author;
+        PublicationDate = _PublicationDate;
+        Price = _Price;
+    }
+    public override string ToString()
+    {
+        return $"{ISBN} - {Title} by {string.Join(", ", Author)} ({PublicationDate.ToShortDateString()}) - ${Price:F2}";
+    }
+}
+
+public static class  BookFunctions
+{
+    public static string GetTitle(Book book) => book.Title;
+    public static string GetAuthors(Book book) => string.Join(", ", book.Author);
+    public static string GetPrice(Book book) => book.Price.ToString();
+}
+
+public delegate string BookDelegate(Book book);
+
+public static class  LibraryEngine
+{
+    public static void ProccessBook(List<Book> bList, Func<Book, string> fPtr)
+    {
+        foreach (Book book in bList)
+            Console.WriteLine(fPtr(book));
+    }
+
+    // ----- hidden for ambiguty whith the above method signature -----
+    //public static void ProccessBook(List<Book> bList, BookDelegate fPtr) 
+    //{
+    //    foreach (Book book in bList)
+    //        Console.WriteLine(fPtr(book));
+    //}
 }
--- obj/Debug/net9.0/CS_ADV_A03.AssemblyInfo.cs
+++ obj/Debug/net9.0/CS_ADV_A03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_ADV_A03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5bf773c8839930ec8428b11fcc520e7e4000fe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_ADV_A03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_ADV_A03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
